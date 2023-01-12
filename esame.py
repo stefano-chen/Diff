3e772c3b178bb7f27362251f8aa25dbb0d8c1378
stefano-chen <stefanochen01@gmail.com>
2023-01-12 16:28:23 +0100

    compute and test

--- esame.py
+++ esame.py
@@ -9,3 +9,17 @@ class Diff:
         if ratio == 0:
             raise ExamException("Ratio cannot be 0")
         self.ratio = ratio
+
+    def compute(self, data: list[int | float]):
+        if not isinstance(data, list):
+            raise ExamException("data must be a list")
+        if not all(isinstance(item, int | float) for item in data):
+            raise ExamException("Not numeric values in data")
+        if len(data) < 2:
+            raise ExamException("Insufficient number of data")
+
+        result = []
+        for i, item in enumerate(data[0:-1]):
+            result.append((data[i + 1] - item)/self.ratio)
+        return result
+
--- test_esame.py
+++ test_esame.py
@@ -9,6 +9,38 @@ class MyTestCase(unittest.TestCase):
     def test_init_zero(self):
         self.assertRaises(ExamException, Diff, 0)
 
+    def test_compute_empty_list(self):
+        x = Diff()
+        self.assertRaises(ExamException, x.compute, [])
+
+    def test_compute_one_element_list(self):
+        x = Diff()
+        self.assertRaises(ExamException, x.compute, [1])
+
+    def test_compute_not_a_list(self):
+        x = Diff()
+        self.assertRaises(ExamException, x.compute, 1)
+
+    def test_compute_list_of_str(self):
+        x = Diff()
+        self.assertRaises(ExamException, x.compute, [1, 1.1, 'Hello'])
+
+    def test_compute(self):
+        x = Diff()
+        self.assertEqual(x.compute([2, 4, 8, 16]), [2, 4, 8])
+
+    def test_compute_negative_ratio(self):
+        x = Diff(-1)
+        self.assertEqual(x.compute([2, 4, 8, 16]), [-2, -4, -8])
+
+    def test_compute_double_ratio(self):
+        x = Diff(2)
+        self.assertEqual(x.compute([2, 4, 8, 16]), [1, 2, 4])
+
+    def test_compute_float_ratio(self):
+        x = Diff(0.5)
+        self.assertEqual(x.compute([2, 4, 8, 16]), [4, 8, 16])
+
 
 if __name__ == '__main__':
     unittest.main()
